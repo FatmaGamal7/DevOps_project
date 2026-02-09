b107bb2b266e1ebf15687eaa82209b988cdb2805
fatmagamal7 <f4tm47@gmail.com>
2026-02-09 16:48:00 +0200

    deploy app on nlb check

--- terraform/modules/EKS/output.tf
+++ terraform/modules/EKS/output.tf
@@ -6,11 +6,3 @@ output "cluster_arn" {
   value = aws_eks_cluster.this.arn
 }
 
-data "aws_eks_node_group" "managed" {
-  cluster_name    = aws_eks_cluster.this.name
-  node_group_name = aws_eks_node_group.managed.node_group_name
-}
-
-output "node_group_sg_id" {
-  value = data.aws_eks_node_group.managed.resources[0].security_groups[0]
-}
