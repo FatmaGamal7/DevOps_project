4bec533ac4a5386c029d2384ba436f161d0d597a
fatmagamal7 <f4tm47@gmail.com>
2026-02-09 21:02:11 +0200

    try nlb with nlb AWS controller1

--- terraform/modules/OIDC/main.tf
+++ terraform/modules/OIDC/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_openid_connect_provider" "this" {
   url             = var.oidc_provider_url
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["..."] # حطي thumbprint الصحيح
+  thumbprint_list = ["..."] 
 }
